4d297fc65463ab23195525a9ff4a81a4fc39f7b9
Raffael Schmid <raffael@yux.ch>
2016-04-28 23:20:30 +0200

    fix specs for route locale

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -12,28 +12,28 @@ RSpec.describe BooksController, type: :controller do
 
   describe 'GET #index' do
     it 'assigns all books as @books' do
-      get :index
+      get :index, locale: :en
       expect(assigns(:books)).to eq([book])
     end
   end
 
   describe 'GET #show' do
     it 'assigns the requested book as @book' do
-      get :show, id: book.to_param
+      get :show, id: book.to_param, locale: :en
       expect(assigns(:book)).to eq(book)
     end
   end
 
   describe 'GET #new' do
     it 'assigns a new book as @book' do
-      get :new
+      get :new, locale: :en
       expect(assigns(:book)).to be_a_new(Book)
     end
   end
 
   describe 'GET #edit' do
     it 'assigns the requested book as @book' do
-      get :edit, id: book.to_param
+      get :edit, id: book.to_param, locale: :en
       expect(assigns(:book)).to eq(book)
     end
   end
@@ -42,30 +42,30 @@ RSpec.describe BooksController, type: :controller do
     context 'with valid params' do
       it 'creates a new book' do
         expect {
-          post :create, book: valid_attributes
+          post :create, book: valid_attributes, locale: :en
         }.to change(Book, :count).by(1)
       end
 
       it 'assigns a newly created book as @book' do
-        post :create, book: valid_attributes
+        post :create, book: valid_attributes, locale: :en
         expect(assigns(:book)).to be_a(Book)
         expect(assigns(:book)).to be_persisted
       end
 
       it 'redirects to the created book' do
-        post :create, book: valid_attributes
+        post :create, book: valid_attributes, locale: :en
         expect(response).to redirect_to(Book.last)
       end
     end
 
     context 'with invalid params' do
       it 'assigns a newly created but unsaved book as @book' do
-        post :create, book: invalid_attributes
+        post :create, book: invalid_attributes, locale: :en
         expect(assigns(:book)).to be_a_new(Book)
       end
 
       it "re-renders the 'new' template" do
-        post :create, book: invalid_attributes
+        post :create, book: invalid_attributes, locale: :en
         expect(response).to render_template('new')
       end
     end
@@ -76,30 +76,30 @@ RSpec.describe BooksController, type: :controller do
       let(:new_attributes) { { title_de: 'new title' } }
 
       it 'updates the requested book' do
-        put :update, id: book.to_param, book: new_attributes
+        put :update, id: book.to_param, book: new_attributes, locale: :en
         book.reload
         expect(book.title_de).to eq('new title')
       end
 
       it 'assigns the requested book as @book' do
-        put :update, id: book.to_param, book: valid_attributes
+        put :update, id: book.to_param, book: valid_attributes, locale: :en
         expect(assigns(:book)).to eq(book)
       end
 
       it 'redirects to the book' do
-        put :update, id: book.to_param, book: valid_attributes
+        put :update, id: book.to_param, book: valid_attributes, locale: :en
         expect(response).to redirect_to(book)
       end
     end
 
     context 'with invalid params' do
       it 'assigns the book as @book' do
-        put :update, id: book.to_param, book: invalid_attributes
+        put :update, id: book.to_param, book: invalid_attributes, locale: :en
         expect(assigns(:book)).to eq(book)
       end
 
       it "re-renders the 'edit' template" do
-        put :update, id: book.to_param, book: invalid_attributes
+        put :update, id: book.to_param, book: invalid_attributes, locale: :en
         expect(response).to render_template('edit')
       end
     end
@@ -109,12 +109,12 @@ RSpec.describe BooksController, type: :controller do
     it 'destroys the requested book' do
       book.save
       expect {
-        delete :destroy, id: book.to_param
+        delete :destroy, id: book.to_param, locale: :en
       }.to change(Book, :count).by(-1)
     end
 
     it 'redirects to the books list' do
-      delete :destroy, id: book.to_param
+      delete :destroy, id: book.to_param, locale: :en
       expect(response).to redirect_to(books_url)
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe UsersController, type: :controller do
   describe 'GET #index' do
     it 'assigns all users as @users' do
       user.save
-      get :index
+      get :index, locale: :en
       expect(assigns(:users)).to eq([user])
     end
   end
 
   describe 'GET #new' do
     it 'assigns a new user as @user' do
-      get :new, {}
+      get :new, locale: :en
       expect(assigns(:user)).to be_a_new(User)
     end
   end
 
   describe 'GET #edit' do
     it 'assigns the requested user as @user' do
-      get :edit, id: user.to_param
+      get :edit, id: user.to_param, locale: :en
       expect(assigns(:user)).to eq(user)
     end
   end
@@ -35,30 +35,30 @@ RSpec.describe UsersController, type: :controller do
     context 'with valid params' do
       it 'creates a new User' do
         expect {
-          post :create, user: valid_attributes
+          post :create, user: valid_attributes, locale: :en
         }.to change(User, :count).by(1)
       end
 
       it 'assigns a newly created user as @user' do
-        post :create, user: valid_attributes
+        post :create, user: valid_attributes, locale: :en
         expect(assigns(:user)).to be_a(User)
         expect(assigns(:user)).to be_persisted
       end
 
       it 'redirects to the created user' do
-        post :create, user: valid_attributes
+        post :create, user: valid_attributes, locale: :en
         expect(response).to redirect_to(users_path)
       end
     end
 
     context 'with invalid params' do
       it 'assigns a newly created but unsaved user as @user' do
-        post :create, user: invalid_attributes
+        post :create, user: invalid_attributes, locale: :en
         expect(assigns(:user)).to be_a_new(User)
       end
 
       it "re-renders the 'new' template" do
-        post :create, user: invalid_attributes
+        post :create, user: invalid_attributes, locale: :en
         expect(response).to render_template('new')
       end
     end
@@ -69,30 +69,30 @@ RSpec.describe UsersController, type: :controller do
       let(:new_attributes) { { email: 'asd@example.org' } }
 
       it 'updates the requested user' do
-        put :update, id: user.to_param, user: new_attributes
+        put :update, id: user.to_param, user: new_attributes, locale: :en
         user.reload
         expect(user.email).to eq('asd@example.org')
       end
 
       it 'assigns the requested user as @user' do
-        put :update, id: user.to_param, user: valid_attributes
+        put :update, id: user.to_param, user: valid_attributes, locale: :en
         expect(assigns(:user)).to eq(user)
       end
 
       it 'redirects to the user' do
-        put :update, id: user.to_param, user: valid_attributes
+        put :update, id: user.to_param, user: valid_attributes, locale: :en
         expect(response).to redirect_to(users_path)
       end
     end
 
     context 'with invalid params' do
       it 'assigns the user as @user' do
-        put :update, id: user.to_param, user: invalid_attributes
+        put :update, id: user.to_param, user: invalid_attributes, locale: :en
         expect(assigns(:user)).to eq(user)
       end
 
       it "re-renders the 'edit' template" do
-        put :update, id: user.to_param, user: invalid_attributes
+        put :update, id: user.to_param, user: invalid_attributes, locale: :en
         expect(response).to render_template('edit')
       end
     end
@@ -101,12 +101,12 @@ RSpec.describe UsersController, type: :controller do
   describe 'DELETE #destroy' do
     it 'destroys the requested user' do
       expect {
-        delete :destroy, id: user.to_param
+        delete :destroy, id: user.to_param, locale: :en
       }.to change(User, :count).by(-1)
     end
 
     it 'redirects to the users list' do
-      delete :destroy, id: user.to_param
+      delete :destroy, id: user.to_param, locale: :en
       expect(response).to redirect_to(users_url)
     end
   end
